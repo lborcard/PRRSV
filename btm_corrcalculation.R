0e17fda0e922dc4de5f70cc6df2db52442423cd6
coilerr <51543572+coilerr@users.noreply.github.com>
2020-06-29 23:17:18 +0200

    changed subtitle

--- btm_corrcalculation.R
+++ btm_corrcalculation.R
@@ -2,6 +2,7 @@ btm_corrcalculation <- function(data_for_corr= superdfadj,
                                 Daystocompare=c(28,3),
                             celltype="%prol. t-cells",
                             genesets,
+                            myvalue="valueadj",
                             treatmentofinterest=c("HP"),
                             corr_value=0.8){
   #
@@ -16,8 +17,9 @@ btm_corrcalculation <- function(data_for_corr= superdfadj,
     filter(day.x==Daystocompare[1])%>%#we set the day we want to use for the FACS data
     filter(day.y==Daystocompare[2])%>%#we set the day we want to use for the enrichment data
     filter(treatment%in%treatmentofinterest)%>%
-    select(animalid,genesets,es,valueadj)%>%
+    select(animalid,genesets,es,myvalue)%>%
     pivot_wider(names_from = genesets,values_from = es)
+  
   
   
   row.names(myfavcorr)<-myfavcorr$animalid
@@ -27,25 +29,37 @@ btm_corrcalculation <- function(data_for_corr= superdfadj,
   res.cor <- correlate(myfavcorr)
   print(head(res.cor))
   res.cor<-res.cor %<>%
-    focus("valueadj")%>%
+    focus(myvalue)%>%
     gather(-rowname, key = "colname", value = "cor") %>% 
-    filter(abs(cor)>corr_value)%>%
+    mutate(rsquared=cor^2)%>%
+    filter(cor^2>corr_value)%>%
+    #filter(rsquared>corr_value)%>%
     print()
   res.cor_btm<-merge(btmfamilies_char,res.cor,by.x = "geneid",by.y = "rowname")
   print(head(res.cor_btm))
     #filter(colname=="value")%>%
   plotrescor<-res.cor_btm %>%
-    filter(NAME!=str_detect(string = NAME,pattern = "TBA"))%>%
+    filter(NAME!=str_detect(string = NAME,
+                            pattern = "TBA"))%>%
     filter(Family_name!="various")%>%
+    filter(Family_name!="cell cycle")%>%
     ggplot(aes(y=NAME, x=Family_name,fill=cor)) +
-
     geom_tile()+
     ggtitle(paste("Day",Daystocompare[1],(celltype)," vs ","Day",Daystocompare[2],"(ES)",paste(treatmentofinterest),sep = ""),
-            subtitle = paste("cutoff p<0.05"," correlation value >abs(",corr_value,")",sep = ""))+
+            subtitle = paste("cutoff p<0.05 ", expression(R^2),">",corr_value,sep = ""))+
     coord_fixed(ratio = 1)+
     
     
     theme_classic()+
+    scale_fill_gradient2(
+      #low = "grey2",
+      #high = "blue",
+      na.value = "grey50",
+      guide = "colourbar",
+      aesthetics = "fill",
+      midpoint=0,
+      limits=c(-1,1)
+    )+
     theme(axis.text.x = element_text(angle = 45,hjust = 1))
     #coord_equal()
   return(plotrescor)
