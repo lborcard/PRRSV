656b6d0a5f20ef34bc4a7679c4009636459db08a
Loïc Borcard <loicborcard@hotmail.com>
2020-08-24 16:41:22 +0200

    added a folder for scripts

--- namesofgenesets.R
+++
@@ -1,5 +0,0 @@
-#extract the names of the genesets
-genesets_ids<-data.frame()
-btmfamilies$geneid<-(str_extract(btmfamilies$NAME,pattern = "(?<=\\()(.\\d.{0,4}?)(?=\\))"))
-genesets_ids$name<-str_remove(genesetsnames,pattern =  "(?<=\\()(.\\d.{0,4}?)(?=\\))")
-library(tidyverse)
